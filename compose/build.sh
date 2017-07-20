89c8a20b7f437efab2ad89c13eb9d4e0a78f47c9
Your Name <you@example.com>
2017-07-20 10:10:26 +0100

    update

--- compose/build.sh
+++ compose/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "Building config-server ----------------->"
+cd ../config-server && ./gradlew clean build
+echo "Building patient-service ----------------->"
+cd ../demo && ./gradlew clean build
 
-cd config-server && ./gradlew clean build
